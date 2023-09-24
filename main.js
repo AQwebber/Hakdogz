33881dd829fd2b09a5343177a38ab2f09e55360d
Arjie.MQ <124896681+AQwebber@users.noreply.github.com>
2023-09-24 16:07:35 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+/*
+var x;
+function menuB(){
+   if(x==1){
+      document.getElementById('menuList').style.display='none';
+      document.getElementById('container').style.backgroundColor='#999955';
+      document.getElementById('pName').style.color='black';
+      x=0;
+   }
+   else{
+      document.getElementById('menuList').style.display='block';
+      document.getElementById('container').style.backgroundColor='black';
+      document.getElementById('pName').style.color='lightgreen';
+      x=1;
+   }
+}*/
+
+document.getElementById('naayIngon').addEventListener("click", function(){
+   alert("Panget ble! HAHA")
+})
